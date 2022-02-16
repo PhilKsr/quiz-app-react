e7872b378474a41acd5b32ac7ea21886a54e8160
Philipp Kaiser <p.kaiser95@gmx.de>
2022-02-16 20:50:36 +0100

    Added server structure

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+const serverPort = process.env.PORT || 4000;
+
+mongoose.connect(
+  `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
+);
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "Mongodb connection error"));
+db.once("open", function (callback) {
+  console.log("Mongodb connection succeeded");
+});
+
+server.listen(serverPort, () =>
+  console.log(`Server is up and running on port ${serverPort}`)
+);
